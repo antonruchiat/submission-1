e38ca1a999df05c8b9a576940a5b4c2b7dfcaafc
TsinmiWaslin95 <57837330+TsinmiWaslin95@users.noreply.github.com>
2019-11-22 08:19:21 +0700

    sayang aplikasinya udah siap

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,13 @@
-const CACHE_NAME = "myfirstsubmission-v8";
+const CACHE_NAME = "myfirstsubmissionv8";
 var urlsToCache = [
     "/",
     "nav.html",
     "/index.html",
     "/footer.html",
+    "/manifest.json",
     "/river.png",
     "/pages/home.html",
-    "/pages/article.html",
-    "/pages/article-1.html",
-    "/pages/article-2.html",
-    "/pages/article-3.html",
+    "/pages/topic.html",
     "/pages/timeline.html",
     "/pages/profile.html",
     "/css/materialize.min.css",
@@ -21,41 +19,37 @@ var urlsToCache = [
     "/img/jan-kopriva-p1SKuYXxqec-unsplash.jpg",
     "/img/levi-saunders-WJQQbVvkzcI-unsplash.jpg",
     "/img/me.jpg",
-    "/img/3_SILENE TOMENTOSA.jpg",
-    "/img/4_675px-Jade_vine_at_Allan_Gardens.jpg",
-    "/img/5_Titan-arum1web.jpg",
-    "/img/8_Rafflesia arnoldii .jpg",
-    "/img/9_Erica_abietina.jpg",
     "/img/download.jpg",
     "/img/2928_main_image.jpg"
 ];
 
 self.addEventListener("install", function(event) {
-    event.waitUntil(
+    event.waitUntil (
         caches.open(CACHE_NAME).then(function(cache) {
             return cache.addAll(urlsToCache);
         })
     );
 });
+
 self.addEventListener("fetch", function(event) {
     event.respondWith(
-      caches
-        .match(event.request, { cacheName: CACHE_NAME })
+        caches
+        .match(event.request, { cacheName : CACHE_NAME })
         .then(function(response) {
-          if (response) {
-            console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
-            return response;
-          }
-   
-          console.log(
-            "ServiceWorker: Memuat aset dari server: ",
-            event.request.url
-          );
-          return fetch(event.request);
+            if (response) {
+                console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+                return response;
+            }
+
+            console.log(
+                "ServiceWorker: Memuat aset dari server: ",
+                event.request.url
+            );
+            return fetch(event.request);
         })
     );
-  });
-  
+});
+
 self.addEventListener("activate", function(event) {
     event.waitUntil(
         caches.keys().then(function(cacheNames) {
